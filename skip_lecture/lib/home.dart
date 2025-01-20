d808b3f7ba6859162582e4f98e0ba20b4fe7fb52
Kidus Mekuria <74838848+kidusm001@users.noreply.github.com>
2025-01-20 20:16:54 +0300

    top bar style done

--- skip_lecture/lib/home.dart
+++ skip_lecture/lib/home.dart
@@ -10,6 +10,30 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 4,
+        title: const Text(
+          "Skip Lecter",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontFamily: "Playwrite IN",
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: IconButton(
+              icon: const Icon(
+                Icons.add,
+                color: Colors.black,
+                size: 28,
+              ),
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
